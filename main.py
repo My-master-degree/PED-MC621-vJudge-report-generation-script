490641385b1a77138b2d44bc76a91dfc497557ed
Luis H. P. Mendes <luishpmendes@gmail.com>
2022-07-20 14:52:45 -0300

    Updating script
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- main.py
+++ main.py
@@ -18,29 +18,167 @@ for i in range(1, n_args):
     if sys.argv[i] == "--verbose":
         VERBOSE = True
 
-#cookies session
-cookies = {
-        'Jax.Q': '',
-        '_gid': '',
-        'JSESSIONID': '',
-        '_ga': ''
-        }
-#current contest files
-contests = [
-        'contests/MC621_MC821_08_11 - Virtual Judge.html',
-        ]
+contests_ids = [
+    479096,
+    479242,
+    480758,
+    481176,
+    481292,
+    487571,
+    488410,
+    491161,
+    491162,
+    496398,
+    496399,
+    497394,
+    498296,
+    499186,
+]
+
 #current username order
 usernames = [
-        'LuizAndia',
-        'b164923',
-        'hboschirolli',
-        ];
+    'hermes_shimizu',
+    'DanielCoimbra',
+    'pirapombo',
+    'GiovanniCDB',
+    'jpsousa',
+    'Thcerbla',
+    'LucasOli',
+    'lumalombello',
+    'Dantas',
+    'ramos',
+    'hiriluk',
+    'humanolaranja',
+    'guilhermeShima',
+    'jhonatas',
+    'joaoguilherme',
+    'j199944',
+    'Lucas_Lopes',
+    'mcnunes',
+    'MatheusOliveira',
+    'maurilio',
+    'oozaku',
+    'AndreLuisRG',
+    'Dorival',
+    'FelipePM01',
+    'gdseabra',
+    'GeorgeGGJ',
+    'kaiotakuma',
+    'LucasFarias',
+    'mariliacss',
+    'MateusOlivi',
+    'MylenaRoberta',
+    'Pedro_Hori',
+    'Desnord',
+    'FabioVillar',
+    'fer20comp',
+    'f234589',
+    'Vinidamasceno',
+    'casotavo',
+    'marcoscunharosa',
+    'mferraz',
+    'mugomes',
+    'thaina',
+    'VPietrobom',
+    'fernandomarques',
+    'PhilipeMS',
+    'caiopetruccirosa',
+    'joaodm423',
+    'lucasfrancisco',
+    'Savitsky',
+    'CristianoSampaio',
+    'Cl3T0',
+    'igorfrade',
+    'ccarneiro',
+    'gmarques.daniel',
+    'victormaruca',
+    'BassaniZ',
+    'QSRafael',
+    'Eric_V',
+    'luizgustavo09',
+    'notGut',
+];
 #usernames list to present
 usernames_to_present = [
-        '<null>',
-        'b164923',
-        'hboschirolli',
-        'LuizAndia',
+    '',
+    '',
+    'hermes_shimizu',
+    'DanielCoimbra',
+    '',
+    'pirapombo',
+    'GiovanniCDB',
+    '',
+    'jpsousa',
+    'Thcerbla',
+    '',
+    '',
+    'LucasOli',
+    'lumalombello',
+    '',
+    'Dantas',
+    '',
+    'ramos',
+    'hiriluk',
+    'humanolaranja',
+    'guilhermeShima',
+    'jhonatas',
+    'joaoguilherme',
+    'j199944',
+    'Lucas_Lopes',
+    'mcnunes',
+    'MatheusOliveira',
+    'maurilio',
+    'oozaku',
+    'AndreLuisRG',
+    'Dorival',
+    'FelipePM01',
+    'gdseabra',
+    'GeorgeGGJ',
+    'kaiotakuma',
+    'LucasFarias',
+    'mariliacss',
+    'MateusOlivi',
+    'MylenaRoberta',
+    'Pedro_Hori',
+    'Desnord',
+    'FabioVillar',
+    'fer20comp',
+    'f234589',
+    'Vinidamasceno',
+    'casotavo',
+    '',
+    'marcoscunharosa',
+    'mferraz',
+    'mugomes',
+    '',
+    'thaina',
+    'VPietrobom',
+    '',
+    'fernandomarques',
+    'PhilipeMS',
+    'caiopetruccirosa',
+    'joaodm423',
+    'lucasfrancisco',
+    'Savitsky',
+    'CristianoSampaio',
+    'Cl3T0',
+    'igorfrade',
+    '',
+    'ccarneiro',
+    'gmarques.daniel',
+    'victormaruca',
+    'BassaniZ',
+    '',
+    'QSRafael',
+    'Eric_V',
+    '',
+    '',
+    '',
+    '',
+    'luizgustavo09',
+    '',
+    '',
+    'notGut',
 ]
 
 usernames_order = {}
@@ -50,18 +188,22 @@ for username in usernames:
     order += 1
 
 #for each contest
-for contest in contests:
+for contest_id in contests_ids:
+    if verbose:
+        print("contest_id: " + str(contest_id))
     #setup
-    f= open(contest,"r") 
-    html =f.read()
-    f.close()
-    parsed_html = BeautifulSoup(html, from_encoding="utf-8")
+    request = requests.get(
+        url = 'https://vjudge.net/contest/'+str(contest_id),
+    ).text
+    parsed_html = BeautifulSoup(request, from_encoding="utf-8")
     students = {}
     jsonData = json.loads(parsed_html.body.find('textarea', attrs = {'name' : "dataJson"}).text)
-    contest_id = jsonData['id']
     n_problems = len(jsonData['problems'])
+    begin_contest_time = jsonData['begin']
     end_contest_time = jsonData['end']
-    print(contest)
+    contest = jsonData['title']
+    if VERBOSE:
+        print(contest)
     #iterate over usernames
     for username in usernames:
         if VERBOSE:
@@ -71,65 +213,77 @@ for contest in contests:
                 'failed': 0, 
                 'rank': 0, 
                 'out_time': 0, 
-                'accepted': 0
+                'accepted': 0,
+                'penalty': 0,
                 }
         for problem in range(n_problems):
             problem_letter = chr(ord('A') + problem)
             if VERBOSE:
-                print("\t", problem_letter)
-            #get request
-            request = requests.get(
-                    url = 'https://vjudge.net/status/data/',
-                    params = {
-                        'start': 0,
-                        'length': 20,
-                        'un': username,
-                        'num': problem_letter,
-                        'res': 1,
-                        'language': '',
-                        'inContest': True,
-                        'contestId': contest_id
-                        },
-                    headers = {
-                        'accept': 'application/json, text/javascript, */*; q=0.01',
-                        'accept-encoding': 'gzip, deflate, br',
-                        'accept-language': 'en-US,en;q=0.9',
-                        'cookie': "_ga="+cookies['_ga']+"; _gid="+cookies['_gid']+"; Jax.Q="+cookies['Jax.Q']+"; JSESSIONID="+cookies['JSESSIONID']+"; _gat=1",
-                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                        'x-requested-with': 'XMLHttpRequest'
-                        }
-                ).text
-            response = json.loads(request)
-            if len(response['data']) == 0:
-                student['failed'] += 1
-            else:
-                valid = False
-                for accepted in response['data']:
-                    if accepted['time'] <= end_contest_time and accepted['userName'].lower() == student['user'].lower():
-                        valid = True
+                print("\t\t", problem_letter)
+            solved = False
+            solving_time = end_contest_time + 1
+            start = 0;
+            while True:
+                request = requests.get(
+                        url = 'https://vjudge.net/status/data/',
+                        params = {
+                            'start': start,
+                            'length': 20,
+                            'un': username,
+                            'num': problem_letter,
+                            },
+                    ).text
+                response = json.loads(request)
+                for data in response['data']:
+                    if data['statusType'] == 0 and data['userName'].lower() == student['user'].lower() and data['contestId'] == contest_id:
+                        solved = True
+                        solving_time = data['time']
                         break
-                if valid:
+                if len(response['data']) == 20:
+                    start += 20
+                else:
+                    break
+            if solved:
+                if solving_time <= end_contest_time:
                     student['accepted'] += 1
                 else:
                     student['out_time'] += 1
+            else:
+                student['failed'] += 1
+            # compute penalty
+            if solved:
+                start = 0;
+                while True:
+                    request = requests.get(
+                            url = 'https://vjudge.net/status/data/',
+                            params = {
+                                'start': start,
+                                'length': 20,
+                                'un': username,
+                                'num': problem_letter,
+                                },
+                        ).text
+                    response = json.loads(request)
+                    for data in response['data']:
+                        if data['userName'].lower() == student['user'].lower() and data['contestId'] == contest_id:
+                            if data['statusType'] == 0:
+                                student['penalty'] += round((data['time']-begin_contest_time)/60000)
+                            else:
+                                student['penalty'] += 20
+                    if len(response['data']) == 20:
+                        start += 20
+                    else:
+                        break
         students[username] = student
     #get username rank
-    rows = parsed_html.body.find('table', attrs={'id': 'contest-rank-table' }).tbody.find_all('tr')
-    rank = 1
-    for row in rows:
-        if rank > 3:
-            break;
-        username = row.find('td', attrs={'class' : 'team'}).div.a.find(text=True, recursive=False).rstrip()
-        if username in students:
-            students[username]['rank'] = rank
-            rank += 1
-    #write csv
+    for username in usernames:
+        students[username]['rank'] = sum((student['accepted'] > students[username]['accepted'] or (student['accepted'] == students[username]['accepted'] and student['penalty'] < students[username]['penalty'])) for student in students.values()) + 1
     with open(contest.split('/')[1] + ".csv", mode='w') as csv_file:
-        fieldnames = ['user', 'accepted', 'out_time', 'rank', 'failed']
+        fieldnames = ['user', 'accepted', 'out_time', 'rank', 'failed', 'penalty']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         for username_to_present in usernames_to_present:
-            row = {'user':username_to_present, 'accepted':0, 'out_time':0, 'rank':'', 'failed':0}
+            row = {'user':username_to_present, 'accepted':0, 'out_time':0, 'rank':'', 'failed':0, 'penalty':0}
             for username in students:
                 if username == username_to_present:
                     row = students[username]
